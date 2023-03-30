98c018a9e05df8a119af3c84d1b8370271c2fed2
lisanyny <110669469+lisanyny@users.noreply.github.com>
2023-03-30 07:13:52 +0000

    ログイン画面の編集

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,12 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.all
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:last_name, :first_name, :order)
+  end
+
 end
