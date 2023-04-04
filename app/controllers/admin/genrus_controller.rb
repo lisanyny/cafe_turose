02942f22ef750318f97e14d7339abb3d74a70ea9
lisanyny <110669469+lisanyny@users.noreply.github.com>
2023-04-04 06:27:18 +0000

    adminのviewの編集

--- app/controllers/admin/genrus_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::GenrusController < ApplicationController
-  def index
-  end
-
-  def edit
-  end
-end
--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -1,8 +1,16 @@
 class Admin::MenusController < ApplicationController
   def index
+    @menus = Menu.all
   end
 
   def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    @menu.save
+    redirect_to admin_menus_path
   end
 
   def show
@@ -10,4 +18,11 @@ class Admin::MenusController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:item, :image_id, :introduction, :price, :is_active, :genre)
+  end
+
 end
