d974bf1870b9edad8e03a52653515ba9cf3ca685
lisanyny <110669469+lisanyny@users.noreply.github.com>
2023-06-15 11:53:00 +0000

    update

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,10 +18,28 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource_or_scope)
+    my_page_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      true && !false
+      redirect_to new_customer_registration_path
+    end
+  end
+
 end
